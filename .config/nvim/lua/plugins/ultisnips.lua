f4f3b57e82b2eff89d02aea715eeae1784cb4944
tenick <usareskenneth22@gmail.com>
2025-08-07 02:09:37 +0800

    add lazy plugin setups, update gitconfig editor (nvim)

---
+++ .config/nvim/lua/plugins/ultisnips.lua
@@ -0,0 +1,10 @@
+return {
+    "SirVer/ultisnips",
+    init = function()
+        vim.g.UltiSnipsExpandTrigger = "<tab>"
+        vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
+        vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
+        vim.g.UltiSnipsEditSplit = "vertical"
+    end,
+}
+
