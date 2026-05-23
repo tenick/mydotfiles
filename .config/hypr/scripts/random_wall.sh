de3ee89a19c6741b25ab375011b580b9799b910a
tenick <usareskenneth22@gmail.com>
2026-05-23 23:27:36 +0800

    update deps; add rolling hash cpp snippet template

--- .config/hypr/scripts/random_wall.sh
+++ .config/hypr/scripts/random_wall.sh
@@ -3,5 +3,5 @@
 WALL_DIR="$HOME/.config/hypr/wallpapers"  # change to your actual path
 IMG=$(find "$WALL_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" \) | shuf -n 1)
 
-swww img "$IMG" --transition-type any --transition-duration 1 --transition-fps 60
+awww img "$IMG" --transition-type any --transition-duration 1 --transition-fps 60
 
